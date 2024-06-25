ac9ae8e08dbb4a17d731acb1f72ebecedd1d4a41
Parceva1 <161861886+Parceva1@users.noreply.github.com>
2024-06-25 17:30:19 +0300

    Add files via upload

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
+from django.conf import settings
 
 app_name = 'blog'
 
@@ -9,3 +10,7 @@ urlpatterns = [
     path('category/<slug:slug>/', views.category_posts, name='category_posts'),
     path('posts/<slug:pk>/', views.post_detail, name='post_detail'),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
